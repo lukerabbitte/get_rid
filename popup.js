90c5989875fe3ffa351e3f8aacdcf1338effcbd6
lukerabbitte <luke.rabbitte@gmail.com>
2023-07-14 14:20:56 -0400

    Getting location name

--- popup.js
+++ popup.js
@@ -22,16 +22,32 @@ function getLocationName() {
   if (navigator.geolocation) {
 
     navigator.geolocation.getCurrentPosition(async (position) => {
-      const { latitude, longitude } = position.coords;
 
-      const response = await fetch(`https://geocode.xyz/${latitude},${longitude}?json=1`);
+      const { latitude, longitude } = position.coords;
+      const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng= ${latitude},${longitude}&key=AIzaSyCEhVHgTYBu8jnphrWEMZH4Jb-yM-4rmQE`);
       const data = await response.json();
 
-      const location = data.city || data.region;
+      // Access first result
+      if (data.results.length === 0) {
+        console.log("No results found.");
+      }
+      else {
+        const result = data.results[0];
 
-      console.log("User's location:", location);
+        // Filter components of the address that we want
+        const requiredTypes = ["neighborhood", "sublocality", "locality", "administrative_area_level_7", "administrative_area_level_6", "administrative_area_level_5", "administrative_area_level_4", "administrative_area_level_3", "administrative_area_level_2", "administrative_area_level_1", "country"];
+
+        const placeName = result.address_components.find(component =>
+          component.types.some(type => requiredTypes.includes(type))
+        )?.long_name || null;
+
+        const placeNameElement = document.getElementById("placeName");
+        placeNameElement.textContent = placeName;
+        return placeName;
+      }
     });
-  } else {
+  }
+  else {
     console.log('Geolocation is not supported');
   }
 }
