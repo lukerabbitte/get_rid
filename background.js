d42cf4ab09fa3d2f1f833ae26b538ab4585af328
lukerabbitte <luke.rabbitte@gmail.com>
2023-07-16 14:39:39 -0400

    Removed unused files

--- background.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.tabs.query({ currentWindow: true }, (tabs) => {
-      if (tabs.length > 0) {
-        const firstTab = tabs[0];
-        chrome.storage.local.set({ savedTab: { url: firstTab.url, title: firstTab.title } });
-      }
-    });
-  });
-  
